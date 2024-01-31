9d13d4e923aa9748caf3b939cb4e55818c69d5e0
zhenyapavlovsky <99541081+zhenyapavlovsky@users.noreply.github.com>
2024-01-31 22:33:20 +0100

    someChanges

---
+++ TestProject /TestProjectTests/Flows/Coordinator/Home/HomeCoordinatorTests.swift
@@ -0,0 +1,14 @@
+//
+//  HomeCoordinatorTests.swift
+//  TestProjectTests
+//
+//  Created by Roman Sorochak on 31.01.2024.
+//
+
+import XCTest
+import TestProject
+
+class HomeCoordinatorTests: XCTestCase {
+    
+    func testRouteToProfileView() {}
+}
---
+++ TestProject /TestProjectTests/Flows/Coordinator/Home/HomeCoordinatorView.swift
@@ -0,0 +1,8 @@
+//
+//  HomeCoordinatorView.swift
+//  TestProjectTests
+//
+//  Created by Roman Sorochak on 31.01.2024.
+//
+
+import Foundation
